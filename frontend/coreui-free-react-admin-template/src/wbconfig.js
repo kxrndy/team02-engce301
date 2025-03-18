18584b18889ea612b9437cddeebf7929c2bc2007
9Kritsada <kritsada.wiriya@gmail.com>
2025-03-18 16:02:16 +0700

    Update frontend

--- frontend/coreui-free-react-admin-template/src/wbconfig.js
+++ frontend/coreui-free-react-admin-template/src/wbconfig.js
@@ -1,19 +1,19 @@
 const wbconfig = {
   development: {
-      hosturl  : 'https://172.17.207.171:4000/api',
-      wsurl    : 'wss://172.17.207.171:4000',
-      masterKey     : 'wallboardapi',
-      clientKey     : 'wallboardapi',
-      javascriptKey : 'wallboardapi',
-      appId : "wallboardapi"
+    hosturl: 'https://localhost:5002/api',
+    wsurl: 'wss://localhost:5002',
+    masterKey: 'wallboardapi',
+    clientKey: 'wallboardapi',
+    javascriptKey: 'wallboardapi',
+    appId: 'wallboardapi',
   },
   production: {
-      hosturl  : 'https://lab-parse-server.se-rmutl.net/api',
-      wsurl    : 'wss://lab-parse-server.se-rmutl.net',
-      masterKey     : 'wallboardapi',
-      clientKey     : 'wallboardapi',
-      javascriptKey : 'wallboardapi',
-      appId : "wallboardapi"
-  }
-};
-export default wbconfig;
+    hosturl: 'https://lab-parse-server.cpe-rmutl.net/team02/api',
+    wsurl: 'wss://lab-parse-server.cpe-rmutl.net/team02',
+    masterKey: 'wallboardapi',
+    clientKey: 'wallboardapi',
+    javascriptKey: 'wallboardapi',
+    appId: 'wallboardapi',
+  },
+}
+export default wbconfig[import.meta.env.PROD ? 'production' : 'development']
